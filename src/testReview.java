d1b591bdceb363901b86ac9b3f239f0bb2263587
Jerry Li <jerry.li1248@gmail.com>
2019-10-21 09:05:07 -0700

    adding the star method into the testReview class

--- src/testReview.java
+++ src/testReview.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 public class testReview {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> doubtScore = new ArrayList<>();
+        doubtScore.add(0);
+
+    }
+    public void starRatingDoubtabiility(double score, ArrayList<Integer> doubtScore){
+        if(score == 5){
+            doubtScore.set(0,doubtScore.get(0) + 6);
+        }else if(score >= 4 && score < 5){
+            doubtScore.set(0,doubtScore.get(0) + 3);
+        }else if(score >= 2 && score < 3){
+            doubtScore.set(0,doubtScore.get(0) + 3);
+        }else if(score >= 0 && score < 2){
+            doubtScore.set(0,doubtScore.get(0) + 6);
+        }
     }
 }
